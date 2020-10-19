5e55ec66e2213405abceec5c718d1b1daa7d5133
gowinder <gowinder@hotmail.com>
2020-10-19 13:04:06 +0800

    fix yaml dump

--- mysite/cloudflare_speed/cf_speed.py
+++ mysite/cloudflare_speed/cf_speed.py
@@ -317,7 +317,9 @@ def test():
         file = os.path.join(pathlib.Path(__file__).parent.absolute(), '../../openclash.yaml')
         with open(file, 'w+', encoding='utf-8') as writer:
             #yaml.dump(clash, writer, indent=4, mapping=2, sequence=4)
-            YAML.dump(clash, writer, indent=4, mapping=2, sequence=4)
+            yaml = YAML()
+            yaml.indent(mapping=2, sequence=4, offset=2)
+            yaml.dump(clash, writer)
         
     print('done!')
 

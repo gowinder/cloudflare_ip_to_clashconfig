53c00275d0abef633dd27e576d4d7dc475c9d0ce
gowinder_211 <gowinder@hotmail.com>
2020-11-02 10:03:39 +0000

    fix docker

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,8 +1,7 @@
-#!bash
+#!sh
 
-#if [ $WITH_REDIS -eq 1 ]
-
-/opt/cloud/mysite/manage.py migrate
-nohup /opt/cloud/mysite/manage.py rqworker default > /opt/cloud/rqworker.log 2>&1 &
-/opt/cloud/mysite/manage.py runserver $DJANGO_HOST:$DJANGO_PORT
+python3 /opt/cloud/mysite/manage.py migrate
+python3 /opt/cloud/mysite/manage.py loaddata /opt/cloud/mysite/tester/fixtures/initial_data.yaml
+nohup python3 /opt/cloud/mysite/manage.py rqworker default > /opt/cloud/rqworker.log 2>&1 &
+python3 /opt/cloud/mysite/manage.py runserver $DJANGO_HOST:$DJANGO_PORT
 

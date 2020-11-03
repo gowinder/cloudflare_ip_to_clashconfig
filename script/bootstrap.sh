bed856c61e2135a36c25c2a85aa03c61fe009486
gowinder@pve-wsl <gowinder@hotmail.com>
2020-11-03 16:04:54 +0800

    fix rqscheduler in docker file

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -4,6 +4,6 @@ python3 /opt/cloud/mysite/manage.py migrate
 python3 /opt/cloud/mysite/manage.py loaddata /opt/cloud/mysite/tester/fixtures/initial_job.yaml
 python3 /opt/cloud/mysite/manage.py loaddata /opt/cloud/mysite/tester/fixtures/initial_scheduler.yaml
 nohup python3 /opt/cloud/mysite/manage.py rqworker default > /opt/cloud/rqworker.log 2>&1 &
-nohup rqscheduler --host $RQ_REDIS_HOST --port $RQ_REDIS_PORT --db $RQ_REDIS_DB
+nohup rqscheduler --host $RQ_REDIS_HOST --port $RQ_REDIS_PORT --db $RQ_REDIS_DB > /opt/cloud/rqscheduler.log 2>&1 &
 python3 /opt/cloud/mysite/manage.py runserver $DJANGO_HOST:$DJANGO_PORT
 
